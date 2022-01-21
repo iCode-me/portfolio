d9532fca5ea2a0e86f7430ab83f480a11b8a3ef6
Allan Shrestha <stevevai111@hotmail.com>
2022-01-21 08:46:31 +0000

    Animation on homepage refined

--- dist/app.dev.js
+++ dist/app.dev.js
@@ -2,12 +2,13 @@
 
 gsap.from('.text', {
   opacity: 0,
-  duration: 1.5,
+  duration: 2,
   y: -50,
   ease: 'Power2.easeInOut'
 });
 gsap.from('.image', {
   duration: 1.5,
   y: 30,
-  opacity: 0
+  opacity: 0,
+  delay: 2
 });
