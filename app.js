fd66d82411a7448bd564dcd3534b1ea4a8d9fa08
Allan Shrestha <stevevai111@hotmail.com>
2022-01-21 08:36:15 +0000

    Animation added to the homepage

--- app.js
+++ app.js
@@ -1,2 +1,3 @@
 
-gsap.from('')
+gsap.from('.text', {opacity: 0, duration: 1.5, y: -50, ease: 'Power2.easeInOut'})
+gsap.from('.image', {duration: 1.5, y:30, opacity: 0})
--- dist/app.dev.js
+++ dist/app.dev.js
@@ -1,3 +1,13 @@
 "use strict";
 
-gsap.from('');
+gsap.from('.text', {
+  opacity: 0,
+  duration: 1.5,
+  y: -50,
+  ease: 'Power2.easeInOut'
+});
+gsap.from('.image', {
+  duration: 1.5,
+  y: 30,
+  opacity: 0
+});
